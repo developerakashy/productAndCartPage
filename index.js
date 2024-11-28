df13b441e71064dc57f17bb3fc9911e679a09598
developerakashy <developerakashy@gmail.com>
2024-11-28 12:43:44 +0530

    pricing fixed upto 2 decimal

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ document.addEventListener('DOMContentLoaded', () => {
             incDecDiv.dataset.productId = product.id
             incDecDiv.innerHTML = `
             <button class="dec"><svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2"><path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/></svg></button>
-            <p class="count">${cartProduct?.quantity > 0 ? cartProduct?.quantity : `0${cartProduct?.quantity}`}</p>
+            <p class="count">${cartProduct?.quantity > 9 ? cartProduct?.quantity : `0${cartProduct?.quantity}`}</p>
             <button class="inc"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 10 10"><path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/></svg></button>
             `
 
@@ -92,7 +92,7 @@ document.addEventListener('DOMContentLoaded', () => {
             productInfoDiv.innerHTML = `
                 <p class="product-name">${product.category}</p>
                 <p class="product-fullname">${product.name}</p>
-                <p class="product-price">$${product.price}</p>
+                <p class="product-price">$${product.price.toFixed(2)}</p>
             `
 
             productDiv.appendChild(productViewDiv)
